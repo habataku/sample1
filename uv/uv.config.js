510b29a301dbe17ecc25f9fefd9a13c727bf0f5e
Thisisan_althowcool <122313708+Thisisanalthowcool@users.noreply.github.com>
2024-02-27 08:03:33 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
-    prefix: '/service/',
-    bare: '/bare/',
+    prefix: '/education/',
+    bare: 'https://iloveeducation.thenubrokers.com/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
